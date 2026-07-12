ed808889d62f79627c4350898ff358a88ef2e8ce
Gabriel-Oliveira0611 <gabriel.pereira.0694@outlook.com>
2026-07-11 22:19:57 -0300

    Décimo sexto dia de estudo em front-end. Focado em aprimorar conhecimentos em CSS com base em media queries e flexbox e a introdução ao DOM em Javascript

---
+++ javascript/aula33 - DOM - manipulacao/assets/js/index.js
@@ -0,0 +1,46 @@
+function addContact() {
+  const contactSection = document.getElementById("contacts-list");
+
+  const h3 = document.createElement("h3");
+  h3.innerText = "Contato";
+
+  const ul = document.createElement("ul");
+
+  const nameLi = document.createElement("li");
+  nameLi.innerText = "Nome: ";
+  const nameInput = document.createElement("input");
+  nameInput.type = "text";
+  nameInput.name = "name";
+
+  nameLi.appendChild(nameInput);
+  ul.appendChild(nameLi);
+  ul.appendChild(document.createElement("br"));
+
+  const phoneLi = document.createElement("li");
+  phoneLi.innerText = "Teleone: ";
+  const phoneInput = document.createElement("input");
+  phoneInput.type = "text";
+  phoneInput.name = "tel";
+
+  phoneLi.appendChild(phoneInput);
+  ul.appendChild(phoneLi);
+  ul.appendChild(document.createElement("br"));
+
+  const addressLi = document.createElement("li");
+  addressLi.innerHTML = '<label for="address">Endereço: </label>';
+  const addressInput = document.createElement("input");
+  addressInput.type = "text";
+  addressInput.name = "address";
+  addressInput.id = "address";
+
+  addressLi.appendChild(addressInput);
+  ul.appendChild(addressLi);
+  ul.appendChild(document.createElement("br"));
+
+  contactSection.append(h3, ul);
+}
+
+function removeContact() {
+  const contactSection = document.getElementById("contacts-list");
+
+}
